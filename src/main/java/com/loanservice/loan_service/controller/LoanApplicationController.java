05e818cf804dbb536f1013e003f09bd4386b97a8
kamana1997 <104790201+kamana1997@users.noreply.github.com>
2026-03-05 09:06:27 +0530

    Add business logic

--- src/main/java/com/loanservice/loan_service/controller/LoanApplicationController.java
+++ src/main/java/com/loanservice/loan_service/controller/LoanApplicationController.java
@@ -2,6 +2,7 @@ package com.loanservice.loan_service.controller;
 
 import com.loanservice.loan_service.domain.model.LoanApplicationRequest;
 import com.loanservice.loan_service.domain.model.LoanApplicationResponse;
+import com.loanservice.loan_service.service.LoanApplicationService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/applications")
 public class LoanApplicationController {
 
+    private final LoanApplicationService loanApplicationService;
+
+    public LoanApplicationController(LoanApplicationService loanApplicationService) {
+        this.loanApplicationService = loanApplicationService;
+    }
+
     @PostMapping
     public ResponseEntity<LoanApplicationResponse> createLoanApplication(
             @Valid @RequestBody LoanApplicationRequest loanApplicationRequest) {
-        return null;
+        return ResponseEntity.ok(loanApplicationService.evaluate(loanApplicationRequest));
     }
 }
